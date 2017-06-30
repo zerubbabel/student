8daa529e102278ea2c5c11d1dd024df371672623
unknown <imate818@gmail.com>
2017-06-30 14:09:30 +0800

    semester 0.1

--- public/student/index.js
+++ public/student/index.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
 
 function select_item(ele){
 	$(ele).toggleClass("bgSelect");
+	var box=$(ele)[0].children[0].children[0]
+	var f=box.checked;
+	box.checked=!f;
+}
+
+function selectAll(ele){
+	var f=$(ele)[0].checked;
+	$("tbody input").each(function(){
+		this.checked=f;	
+	});
+
 }
